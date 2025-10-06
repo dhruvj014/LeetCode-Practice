561ba9d6f0e4ad3637782b47629bec6cb30cc7b2
Dhruv Jain <95852713+dhruvj014@users.noreply.github.com>
2025-10-06 07:15:22 -0700

    0735-ASTEROID-COLLISION Stats: Time: 7 ms (59.77%), Space: 19.1 MB (19.89%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -5,17 +5,10 @@ class Solution:
             if asteroids[i]>0:
                 st.append(asteroids[i])
             else:
-                destroyed = False
-                while len(st)>0 and st[-1]>0 and st[-1]<=abs(asteroids[i]):
-                    if st[-1] == abs(asteroids[i]):
-                        destroyed = True
-                        st.pop()
-                        break
-                    else:
-                        st.pop()
-                if len(st)>0 and st[-1] > abs(asteroids[i]):
-                    continue
-                print("currently on - "+str(asteroids[i]))
-                if not destroyed:
+                while len(st)>0 and st[-1]>0 and st[-1]<abs(asteroids[i]):
+                    st.pop()
+                if len(st)>0 and st[-1] == abs(asteroids[i]):
+                    st.pop()
+                elif(not st or st[-1]<0):
                     st.append(asteroids[i])
         return st
