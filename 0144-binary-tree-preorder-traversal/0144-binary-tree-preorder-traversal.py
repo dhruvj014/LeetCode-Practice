d61f717d3bad327885c402f86b6f5904b0124c26
Dhruv Jain <95852713+dhruvj014@users.noreply.github.com>
2025-10-26 16:46:39 -0700

    0144-BINARY-TREE-PREORDER-TRAVERSAL Stats: Time: 0 ms (100%), Space: 17.9 MB (34.4%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -6,17 +6,25 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Morris Preorder Traversal
         preorder = []
-        if root is None:
-            return preorder
-        st = []
-        st.append(root)
-        while st:
-            node = st[-1]
-            st.pop()
-            preorder.append(node.val)
-            if node.right is not None:
-                st.append(node.right)
-            if node.left is not None:
-                st.append(node.left)
-        return preorder               
+        curr = root
+        while curr:
+            if not curr.left:
+                preorder.append(curr.val)
+                curr = curr.right
+            else:
+                prev = curr.left
+                while(prev.right and prev.right != curr):
+                    prev = prev.right
+                
+                if not prev.right:
+                    prev.right = curr
+                    # changed this append to be here
+                    preorder.append(curr.val)
+                    curr = curr.left
+                else:
+                    prev.right = None
+                    # instead of being here
+                    curr = curr.right
+        return preorder
