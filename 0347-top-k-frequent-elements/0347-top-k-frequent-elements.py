db260799cef07655accb0bd10616221119a9868c
Dhruv Jain <95852713+dhruvj014@users.noreply.github.com>
2025-10-31 08:16:55 -0700

    0347-TOP-K-FREQUENT-ELEMENTS Stats: Time: 3 ms (90.46%), Space: 21.3 MB (61.87%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
-        n = len(nums)
-        for i in range(n):
-            hashmap[nums[i]] = hashmap.get(nums[i],0) + 1
-        hashmap = sorted(hashmap.items(), key=lambda item: item[1], reverse=True)
-        res = []
-        for i, key in enumerate(hashmap):
-            if i < k:
-                res.append(key[0])
-            else:
-                break
-        return res
+        freq = Counter(nums)
+        minHeap = []
+        for num, count in freq.items():
+            heapq.heappush(minHeap, (count, num))
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+        return [num for count, num in minHeap]
