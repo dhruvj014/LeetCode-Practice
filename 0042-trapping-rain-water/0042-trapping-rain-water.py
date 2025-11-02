cddb5b633dc40ac8b6194eb34a08f67744d67042
Dhruv Jain <95852713+dhruvj014@users.noreply.github.com>
2025-11-01 17:58:11 -0700

    0042-TRAPPING-RAIN-WATER Stats: Time: 14 ms (54.32%), Space: 19.2 MB (69.45%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,27 @@
 class Solution:
     def trap(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        leftMax = nums[l]
+        rightMax = nums[r]
         total = 0
-        n = len(nums)
-        prefix = [0]*n
-        prefix[0] = nums[0]
-        for i in range(1,n):
-            prefix[i] = max(prefix[i-1],nums[i])
-        suffixMax = [0]*n
-        suffixMax[n-1] = nums[n-1]
-        for i in range(n-2,-1,-1):
-            suffixMax[i] = max(suffixMax[i+1],nums[i])
-
-        for i in range(n):
-            leftMax = prefix[i]
-            rightMax = suffixMax[i]
-            if(nums[i]<leftMax and nums[i]<rightMax):
-                total+= min(leftMax,rightMax) - nums[i]
+        while l<r:
+            if leftMax < rightMax:
+                calc = leftMax - nums[l]
+                if calc>0:
+                    total += calc
+                leftMax = max(leftMax, nums[l])
+                if leftMax < rightMax:
+                    l+=1
+                else:
+                    r-=1
+            else:
+                calc = rightMax - nums[r]
+                if calc>0:
+                    total += calc
+                rightMax = max(rightMax, nums[r])
+                if leftMax < rightMax:
+                    l+=1
+                else:
+                    r-=1
         return total
