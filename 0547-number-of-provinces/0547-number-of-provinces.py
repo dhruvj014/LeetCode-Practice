c815eaf6cbea1999d52a317469d135f4b93d833d
Dhruv Jain <95852713+dhruvj014@users.noreply.github.com>
2025-11-09 14:29:10 -0800

    0547-NUMBER-OF-PROVINCES Stats: Time: 16 ms (12.66%), Space: 18.9 MB (70.79%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,24 +1,35 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
+
+    def findParent(self, node):
+        while node != self.parent[node]:
+            self.parent[node] = self.parent[self.parent[node]]
+            node = self.parent[node]
+        return node
+
+    def unionBySize(self, u, v):
+        pu, pv = self.findParent(u), self.findParent(v)
+        if pu == pv:
+            return
+        if self.size[pu] < self.size[pv]:
+            self.parent[pu] = pv
+            self.size[pv] += self.size[pu]
+        else:
+            self.parent[pv] = pu
+            self.size[pu] += self.size[pv]
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def mydfs(node):
-            visited[node] = 1
-            print("for node - "+str(node)+" the neighbours are "+str(adj[node]))
-            for neighbour in adj[node]:
-                if not visited[neighbour]:
-                    mydfs(neighbour)
-        n = len(isConnected[0])
-        prov = 0
-        
-        adj = [[] for _ in range(n)]
-        for i in range(len(isConnected)):
-            for j in range(n):
-                if isConnected[i][j] == 1 and i != j:
-                    adj[i].append(j)
-                    adj[j].append(i)
-
-        visited = [0]*n
+        n = len(isConnected)
+        ds = DisjointSet(n)
         for i in range(n):
-            if visited[i] == 0:
-                mydfs(i)
-                prov += 1
-        return prov
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    ds.unionBySize(i,j)
+        ctr = 0
+        for i in range(n):
+            if ds.findParent(i) == i:
+                ctr+=1
+        return ctr
