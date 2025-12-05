d5e8d2e7fc8fd5d19846d199f3471ef961ee6992
Dhruv Jain <95852713+dhruvj014@users.noreply.github.com>
2025-12-05 06:46:15 -0800

    0739-DAILY-TEMPERATURES Stats: Time: 140 ms (15.31%), Space: 33.3 MB (30.63%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,7 +2,7 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         st = []
         sol = [0]*len(temperatures)
-        st.append((temperatures[0],0))
+        # st.append((temperatures[0],0))
         for i in range(len(temperatures)):
             prev = st[-1][0 ] if st else 0
             if temperatures[i] > prev and prev != 0:
