282214bb76d4ac70971ac739c53cd6f8d89b687b
Dhruv Jain <95852713+dhruvj014@users.noreply.github.com>
2026-02-27 15:13:41 -0800

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 83 ms (65.57%), Space: 19.7 MB (75.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,15 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        l,r,maxlen,maxf = 0,0,0,0
+        hasher = {}
+        l,r,maxf,maxlen = 0,0,0,0
         hasher = [0]*26
-        while(r<len(s)):
-            hasher[ord(s[r])-ord('A')]+=1
+        while r<len(s):
+            hasher[ord(s[r]) - ord('A')] += 1
             maxf = max(maxf, hasher[ord(s[r]) - ord('A')])
-            if((r-l+1) - maxf > k):
-                hasher[ord(s[l]) - ord('A')]-=1
-                maxf = 0
-                l+=1
-            if((r-l+1)-maxf<=k):
+            if ((r-l+1) - maxf > k):
+               hasher[ord(s[l]) - ord('A')] -= 1
+               l+=1
+               maxf = 0
+            else:
                 maxlen = max(maxlen,r-l+1)
             r+=1
         return maxlen
